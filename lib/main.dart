a3bebbe3369cb1e59a7a3d7d5989b2f79f17c9fc
Yogesh070 <ythapa430@gmail.com>
2025-05-08 05:23:57 +0545

    add: riverpod setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const TomatoApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TomatoApp extends StatelessWidget {
+  const TomatoApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pomodoro App',
+      title: 'Tomato App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Text('Pomodoro App'),
+      home: const Text('Tomato App'),
+    );
+  }
+}
     );
   }
 }
