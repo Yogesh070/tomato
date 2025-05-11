a2be3ca4867bc38ae4e98cb762760813b3979647
Yogesh070 <ythapa430@gmail.com>
2025-05-11 09:38:21 +0545

    update: main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tomato/screens/pomodoro.dart';
+import 'package:tomato/screens/report.dart';
+import 'package:tomato/screens/setting.dart';
+import 'package:tomato/screens/task.dart';
 
 void main() {
   runApp(ProviderScope(child: const TomatoApp()));
@@ -12,12 +16,92 @@ class TomatoApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tomato App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.grey,
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(color: Colors.white),
+          bodyLarge: TextStyle(color: Colors.white70),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+        cardTheme: CardTheme(
+          elevation: 0,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          color: Color(0xFF121212),
+        ),
       ),
-      home: const Text('Tomato App'),
+      home: const HomeScreen(),
     );
   }
 }
+
+final fullscreenModeProvider = StateProvider<bool>((ref) => false);
+
+class HomeScreen extends ConsumerStatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _screens = [
+    PomodoroScreen(),
+    TasksScreen(),
+    ReportsScreen(),
+    SettingsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isInFullscreenMode = ref.watch(fullscreenModeProvider);
+
+    if (isInFullscreenMode) {
+      return const PomodoroScreen();
+    }
+
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey.shade700,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timer_outlined),
+            label: 'Timer',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.check), label: 'Tasks'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart_outlined),
+            label: 'Stats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings_outlined),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
